39c9a00b39f9c0327e17f4e443c6a3f249c4fbaf
Oligo-Webdesign <marc.hahn@oligo.de>
2015-03-25 14:41:36 +0100

    search-filter

--- js/main.js
+++ js/main.js
@@ -1,24 +1,20 @@
 // Parallax Scrolling
-$(window).scroll(function(e){
-  parallax();
-});
-
-function parallax(){
-  var scrolled = $(window).scrollTop();
-  $('.hero').css('top',-(scrolled*0.0515)+'rem');
-  $('.hero > div').css('top',-(scrolled*-0.005)+'rem');
-  $('.hero > div ').css('opacity',1-(scrolled*.001));
-};
-
+    $(window).scroll(function(e){
+    parallax();
+    });
+    function parallax(){
+        var scrolled = $(window).scrollTop();
+            $('.hero').css('top',-(scrolled*0.0515)+'rem');
+            $('.hero > div').css('top',-(scrolled*-0.005)+'rem');
+            $('.hero > div ').css('opacity',1-(scrolled*.001));
+    };
 
 // Scroll to top
     $('.scrolltop').click(function(e)
     {
-        $('html, body').animate({'scrollTop': 0}, 1500);
-        e.preventDefault();
+      $('html, body').animate({'scrollTop': 0}, 1500);
+      e.preventDefault();
     });
-
-
 
 // Hero Slider
     $.global = new Object();
@@ -27,46 +23,49 @@ function parallax(){
     $.global.total = 3;
 
     $(document).ready(function() 
-        {
-        
-       $("#slides li:nth-child(1)").removeClass('buried');
-        
-        var SlideCount = $('#slides li').length;
-        
-       $.global.total = SlideCount; 
+      {
+      
+     $("#slides li:nth-child(1)").removeClass('buried');
+      
+      var SlideCount = $('#slides li').length;
+      
+     $.global.total = SlideCount; 
 
-      $('#arrow-left').click(function() { Slide('back'); }); 
-      $('#arrow-right').click(function() { Slide('forward'); }); 
-        
-      });
+    $('#arrow-left').click(function() { Slide('back'); }); 
+    $('#arrow-right').click(function() { Slide('forward'); }); 
+      
+    });
 
     function Slide(direction)
-        {
-       
-        if (direction == 'back') { var $target = $.global.item - 1; }
-        if (direction == 'forward') { var $target = $.global.item + 1; }  
-        
-        if ($target == 0) { DoIt($.global.total); } 
-        else if ($target > $.global.total) { DoIt(1); }  
-        else { DoIt($target); }    
-        }
-
-    function DoIt(target)
       {
-      $('.slide').addClass('buried');
-        $.global.item = target; 
-      var $newtarget = $("#slides li:nth-child("+target+")");
-      $newtarget.removeClass('buried');
+      if (direction == 'back') { var $target = $.global.item - 1; }
+      if (direction == 'forward') { var $target = $.global.item + 1; }
+      if ($target == 0) { DoIt($.global.total); } 
+      else if ($target > $.global.total) { DoIt(1); }  
+      else { DoIt($target); }    
       }
 
+    function DoIt(target)
+    {
+    $('.slide').addClass('buried');
+      $.global.item = target; 
+    var $newtarget = $("#slides li:nth-child("+target+")");
+    $newtarget.removeClass('buried');
+    }
 
+// Overflow
     $('body').css({
-    'overflow-x': 'scroll'
-});
+      'overflow-x': 'scroll'
+    });
 
+// Arrow Collapse
+    $('.collapse').on('shown.bs.collapse', function(){
+      $(this).parent().find(".fa-angle-down").removeClass("fa-angle-down").addClass("fa-angle-up");
+        }).on('hidden.bs.collapse', function(){
+        $(this).parent().find(".fa-angle-up").removeClass("fa-angle-up").addClass("fa-angle-down");
+    });
 
-
-// Filter
+// FILTER  
     var width = $('.filter').width();
     $('.open').hide();
     $('.close').click(function(){
@@ -79,56 +78,49 @@ function parallax(){
         $('.close').show();
         $(this).hide();
     });
-
-    
-
-
-// Arrow Collapse
-$('.collapse').on('shown.bs.collapse', function(){
-  $(this).parent().find(".fa-angle-down").removeClass("fa-angle-down").addClass("fa-angle-up");
-    }).on('hidden.bs.collapse', function(){
-    $(this).parent().find(".fa-angle-up").removeClass("fa-angle-up").addClass("fa-angle-down");
-});
-
-
-
-
-// FILTER  
-  // Price
-  $(function() {
-    $( "#slider-price" ).slider({
-      range: true,
-      min: 0,
-      max: 3500,
-      values: [ 75, 3000 ],
-        slide: function( event, ui ) {
-            $( "#amount" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
-        }
-    });
-    $( "#amount" ).val( "€" + $( "#slider-price" ).slider( "values", 0 ) +
-    " - €" + $( "#slider-price" ).slider( "values", 1 ) );
-  });
-  // Price
-  $(function() {
-    $( "#slider-price" ).slider({
-      range: true,
-      min: 0,
-      max: 3500,
-      values: [ 75, 3000 ],
-        slide: function( event, ui ) {
-            $( "#amount" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
-        }
-    });
-    $( "#amount" ).val( "€" + $( "#slider-price" ).slider( "values", 0 ) +
-    " - €" + $( "#slider-price" ).slider( "values", 1 ) );
-  });
-
-
-
-
-
-
-
-
-
-
+      // Price
+          $(function() {
+            $( "#slider-price" ).slider({
+              range: true,
+              min: 0,
+              max: 3500,
+              values: [ 0, 500 ],
+                slide: function( event, ui ) {
+                    $( "#price" ).val( "€" + ui.values[ 0 ] + " - €" + ui.values[ 1 ] );
+                }
+            });
+            $( "#price" ).val( "€" + $( "#slider-price" ).slider( "values", 0 ) +
+            " - €" + $( "#slider-price" ).slider( "values", 1 ) );
+          });
+      // Lightcolor
+          $(function() {
+            $( "#slider-lightcolor" ).slider({
+              range: true,
+              min: 1000,
+              max: 10000,
+              values: [ 1000, 2000 ],
+                slide: function( event, ui ) {
+                    $( "#lightcolor" ).val( "K" + ui.values[ 0 ] + " - K" + ui.values[ 1 ] );
+                }
+            });
+            $( "#lightcolor" ).val( "K" + $( "#slider-lightcolor" ).slider( "values", 0 ) +
+            " - K" + $( "#slider-lightcolor" ).slider( "values", 1 ) );
+          });
+      // LightPower
+          $(function() {
+            $( "#slider-lightPower" ).slider({
+              range: true,
+              min: 100,
+              max: 5500,
+              values: [ 100, 1000 ],
+                slide: function( event, ui ) {
+                    $( "#lightPower" ).val( "lm" + ui.values[ 0 ] + " - lm" + ui.values[ 1 ] );
+                }
+            });
+            $( "#lightPower" ).val( "lm" + $( "#slider-lightPower" ).slider( "values", 0 ) +
+            " - lm" + $( "#slider-lightPower" ).slider( "values", 1 ) );
+          });
+      // Color Picker
+          $('select[name="colorpicker-fontawesome"]').simplecolorpicker({
+            theme: 'fontawesome',
+          });
