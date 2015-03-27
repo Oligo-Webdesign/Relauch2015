6bf6fb7e04100dbffc643532cc0ade4081bbe9f0
Oligo-Webdesign <marc.hahn@oligo.de>
2015-03-27 15:52:36 +0100

    detail page

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@
     });
     function parallax(){
         var scrolled = $(window).scrollTop();
-            $('.hero').css('top',-(scrolled*0.0515)+'rem');
+            $('.hero').css('top',-(scrolled*0.0215)+'rem');
             $('.hero > div').css('top',-(scrolled*-0.005)+'rem');
             $('.hero > div ').css('opacity',1-(scrolled*.001));
     };
@@ -19,43 +19,6 @@
       $('html, body').animate({'scrollTop': 0}, 1500);
       e.preventDefault();
     });
-
-// Hero Slider
-    $.global = new Object();
-
-    $.global.item = 1;
-    $.global.total = 3;
-
-    $(document).ready(function() 
-      {
-      
-     $("#slides li:nth-child(1)").removeClass('buried');
-      
-      var SlideCount = $('#slides li').length;
-      
-     $.global.total = SlideCount; 
-
-    $('#arrow-left').click(function() { Slide('back'); }); 
-    $('#arrow-right').click(function() { Slide('forward'); }); 
-      
-    });
-
-    function Slide(direction)
-      {
-      if (direction == 'back') { var $target = $.global.item - 1; }
-      if (direction == 'forward') { var $target = $.global.item + 1; }
-      if ($target == 0) { DoIt($.global.total); } 
-      else if ($target > $.global.total) { DoIt(1); }  
-      else { DoIt($target); }    
-      }
-
-    function DoIt(target)
-    {
-    $('.slide').addClass('buried');
-      $.global.item = target; 
-    var $newtarget = $("#slides li:nth-child("+target+")");
-    $newtarget.removeClass('buried');
-    }
 
 // Overflow
     $('body').css({
@@ -129,8 +92,15 @@
             theme: 'fontawesome',
           });
 
-$('#carousel-example-generic').carousel({
-  interval: 2000,
+
+$('#carousel-hero').carousel({
+  interval: 2000
+});
+
+$('#carousel-impression').carousel({
+  interval: 4000,
   keyboard: true
-})
+});
+
+
 
