d027a4c7c2c0f016be00a7daadb6899839a2a02f
Oligo-Webdesign <marc.hahn@oligo.de>
2015-03-31 15:48:36 +0200

    detail page

--- js/main.js
+++ js/main.js
@@ -12,6 +12,17 @@
 
 
 
+ 
+
+
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(300).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+});
+
+
+
 // Hiding Navigation
     $("div.navbar-fixed-top").autoHidingNavbar();
 
